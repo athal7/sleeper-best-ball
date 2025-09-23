1ba18644fc3f4ffc319f8ad451bf92cac88df233
Andrew Thal <467872+athal7@users.noreply.github.com>
2025-09-23 19:04:33 +0000

    Accurately calculate whether the game is over

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sleeper_wrapper import League, User, Stats, Players
 import nfl_data_py as nfl
 from datetime import datetime
-from utils import current_week
+from utils import current_week, is_game_over
 
 st.title("Sleeper Best Ball Outcome Predictor 🏈")
 season = int(st.query_params.get('season', datetime.now().year))
@@ -32,54 +32,62 @@ else:
     league = League(league_id)
     st.subheader(league.get_league_name())
     st.write(f"League ID: {league_id}")
+
     def set_week():
         st.query_params.week = st.session_state.selected_week
-    st.number_input("Enter the week number:", min_value=1, max_value=18, value=week, on_change=set_week, key="selected_week")
+    st.number_input("Enter the week number:", min_value=1, max_value=18,
+                    value=week, on_change=set_week, key="selected_week")
     stats = Stats()
-    week_stats = stats.get_week_stats("regular", season, week)
     week_projections = stats.get_week_projections("regular", season, week)
-    
-    matchups = league.get_matchups(week)    
+
+    matchups = league.get_matchups(week)
     all_players = Players().get_all_players("nfl")
     rosters = league.get_rosters()
     users = league.get_users()
-        
+
     def get_optimistic_score(df):
         # Only consider players who played or have a projection
+        st.dataframe(df)
         df = df.copy()
         df['optimistic'] = df.apply(
             lambda row: row['points'] if row['game_played'] else row['projection'], axis=1
-            )
+        )
         df = df[df['optimistic'].notnull()]
         # Split by position
-        qbs = df[df['position'] == 'QB'].sort_values('optimistic', ascending=False)
-        rbs = df[df['position'] == 'RB'].sort_values('optimistic', ascending=False)
-        wrs = df[df['position'] == 'WR'].sort_values('optimistic', ascending=False)
-        tes = df[df['position'] == 'TE'].sort_values('optimistic', ascending=False)
-        flex = df[df['position'].isin(['RB', 'WR', 'TE'])].sort_values('optimistic', ascending=False)
+        qbs = df[df['position'] == 'QB'].sort_values(
+            'optimistic', ascending=False)
+        rbs = df[df['position'] == 'RB'].sort_values(
+            'optimistic', ascending=False)
+        wrs = df[df['position'] == 'WR'].sort_values(
+            'optimistic', ascending=False)
+        tes = df[df['position'] == 'TE'].sort_values(
+            'optimistic', ascending=False)
+        flex = df[df['position'].isin(['RB', 'WR', 'TE'])].sort_values(
+            'optimistic', ascending=False)
         superflex = df.sort_values('optimistic', ascending=False)
 
         starters = []
-            # 1 QB
+        # 1 QB
         starters += qbs.head(1).to_dict('records')
-            # 3 RB
+        # 3 RB
         starters += rbs.head(3).to_dict('records')
-            # 3 WR
+        # 3 WR
         starters += wrs.head(3).to_dict('records')
-            # 2 TE
+        # 2 TE
         starters += tes.head(2).to_dict('records')
-            # 1 RB/WR/TE FLEX (not already counted)
+        # 1 RB/WR/TE FLEX (not already counted)
         used_ids = {p['player_id'] for p in starters}
         flex_avail = flex[~flex['player_id'].isin(used_ids)]
         starters += flex_avail.head(1).to_dict('records')
         used_ids = {p['player_id'] for p in starters}
-            # 1 SUPERFLEX (QB/RB/WR/TE, not already counted)
+        # 1 SUPERFLEX (QB/RB/WR/TE, not already counted)
         superflex_avail = superflex[~superflex['player_id'].isin(used_ids)]
         starters += superflex_avail.head(1).to_dict('records')
-            # Calculate total
-        total = sum(p['optimistic'] for p in starters if p['optimistic'] is not None)
+        # Calculate total
+        total = sum(p['optimistic']
+                    for p in starters if p['optimistic'] is not None)
         return total, starters
-        
+
     players = []
     matchup_dict = {}
     for matchup in matchups:
@@ -90,21 +98,20 @@ else:
         matchup_dict[matchup_id].append(user_id)
         for player_id, points in matchup['players_points'].items():
             player = all_players.get(player_id, {})
-            player_stats = week_stats.get(player_id, {})
             player_projection = week_projections.get(player_id, {})
             projection = player_projection.get('pts_ppr')
             if player['position'] == 'TE' and projection is not None:
                 projection += player_projection.get('rec', 0) * 0.5
             players.append({
-                    "player_id": player_id,
-                    "player_name": player['full_name'],
-                    "user_id": user_id,
-                    "position": player.get('position'),
-                    "points": points,
-                    "projection": projection,
-                    "game_played": player_stats.get('gp', 0),
-                })
-                
+                "player_id": player_id,
+                "player_name": player['full_name'],
+                "user_id": user_id,
+                "position": player.get('position'),
+                "points": points,
+                "projection": projection,
+                "game_played": is_game_over(schedule, week, player.get('team'))
+            })
+
     players_df = pd.DataFrame(players)
     users_df = pd.DataFrame(users)
     st.header("Matchups")
@@ -113,8 +120,9 @@ else:
         for idx, user_id in enumerate(user_ids):
             team_df = players_df[players_df['user_id'] == user_id]
             optimistic_score, starters = get_optimistic_score(team_df)
-            user_display_name = users_df.loc[users_df['user_id'] == user_id, 'display_name'].values[0]
+            user_display_name = users_df.loc[users_df['user_id']
+                                             == user_id, 'display_name'].values[0]
             cols[idx].markdown(f"**{user_display_name}**")
             cols[idx].write(f"Projected Score: **{optimistic_score:.2f}**")
-            cols[idx].dataframe(pd.DataFrame(starters)[['player_name', 'position', 'optimistic', 'points']].rename(columns={'optimistic': 'projection'}))
-        
+            cols[idx].dataframe(pd.DataFrame(starters)[
+                                ['player_name', 'position', 'optimistic', 'points']].rename(columns={'optimistic': 'projection'}))
--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
 def current_week(schedule):
     return schedule[schedule['result'].isnull()]['week'].min()
+
+
+def is_game_over(schedule, week, team):
+    team_game = schedule[((schedule['home_team'] == team) | (
+        schedule['away_team'] == team)) & (schedule['week'] == week)]
+    team_result = team_game['result'] if not team_game.empty else None
+    return team_result.notnull().all() if team_result is not None else False
