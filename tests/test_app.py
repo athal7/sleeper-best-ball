8aa2c3156447f6a2aa806f153ed7e3ed20492190
Andrew Thal <467872+athal7@users.noreply.github.com>
2025-11-02 15:27:28 +0000

    Fix tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,24 +12,24 @@ def _app():
 def test_by_username_input():
     at = _app().run()
     at.text_input[0].set_value(user).run()
-    assert league in at.markdown[1].value
-    assert league_id in at.markdown[2].value
+    assert league in at.markdown[0].value
+    assert league_id in at.markdown[1].value
 
 
 def test_by_username_query_param():
     at = _app()
     at.query_params['username'] = user
     at.run()
-    assert league in at.markdown[1].value
-    assert league_id in at.markdown[2].value
+    assert league in at.markdown[0].value
+    assert league_id in at.markdown[1].value
 
 
 def test_by_league_query_param():
     at = _app()
     at.query_params['league'] = league_id
     at.run()
-    assert league in at.markdown[1].value
-    assert league_id in at.markdown[2].value
+    assert league in at.markdown[0].value
+    assert league_id in at.markdown[1].value
 
 
 def test_players():
