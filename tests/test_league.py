1176d35587b09ca2cfd2dd54c51656224bfb00f7
Andrew Thal <467872+athal7@users.noreply.github.com>
2025-11-17 18:19:10 +0000

    Remove unused imports

--- tests/test_league.py
+++ tests/test_league.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import tests.mock
-from streamlit_app import League, Player
-from dataclasses import asdict
+from streamlit_app import League
 
 
 def test_points_calculations():
